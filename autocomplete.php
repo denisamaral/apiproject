9640c7e9da7cf76d6048e14f1c8c766c4a4e6eb1
Denis Amaral <denis.amaral@live.com>
2020-06-19 02:53:10 -0300

    Add files via upload

---
+++ autocomplete.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+
+ * @property ClientesApiModel                    	$ClientesApiModel                    	ClientesApiModel
+ */
+class CI_Controller {
+    public function __construct () { }
+}
+
+/**
+
+ * @property ClientesApiModel            $ClientesApiModel            ClientesApiModel
+ */
+class CI_Model {
+    public function __construct () { }
+}
